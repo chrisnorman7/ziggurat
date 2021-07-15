4a92e9abb3d009e9ec6481f024df41557e125348
Chris Norman <chris.norman2@googlemail.com>
2021-07-15 19:06:16 +0100

    Added a walk interval to surfaces.

--- lib/src/tile_types/surface.dart
+++ lib/src/tile_types/surface.dart
@@ -2,4 +2,10 @@
 import 'base.dart';
 
 /// A simple surface that can be walked on.
-class Surface extends TileType {}
+class Surface extends TileType {
+  /// Create a surface.
+  Surface({this.walkInterval = 0.5});
+
+  /// How many seconds must elapse between player footsteps.
+  final double walkInterval;
+}
