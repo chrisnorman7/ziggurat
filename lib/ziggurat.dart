ae3fd4d5cf802da157a5b67c9f1f29a53966e374
Chris Norman <chris.norman2@googlemail.com>
2021-07-25 05:45:15 +0100

    Updated docs.

--- lib/ziggurat.dart
+++ lib/ziggurat.dart
@@ -1,11 +1,20 @@
-/// A library for creating game maps.
+/// A library for creating RPGs.
+///
+/// The notion of game maps is provided by the [Ziggurat] class.
+///
+/// Games can be played with the [Runner] class.
 library ziggurat;
+
+import 'src/runner.dart';
+import 'src/ziggurat.dart';
 
 export 'src/ambiance.dart';
 export 'src/directions.dart';
 export 'src/error.dart';
+export 'src/extensions.dart';
 export 'src/quest.dart';
 export 'src/random_sound.dart';
+export 'src/random_sound_container.dart';
 export 'src/reverb_preset.dart';
 export 'src/runner.dart';
 export 'src/tile.dart';
