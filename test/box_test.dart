05266fbea39b2b87c78cfb1fb0f24f7338229e88
Chris Norman <chris.norman2@googlemail.com>
2021-08-16 05:02:31 +0100

    Fix a test.

--- test/box_test.dart
+++ test/box_test.dart
@@ -33,7 +33,7 @@ void main() {
       expect(t.start, equals(Point<int>(0, 0)));
       expect(t.end, equals(Point<int>(5, 5)));
       expect(t.type is Surface, isTrue);
-      expect(t.type.moveInterval, equals(Duration(milliseconds: 500)));
+      expect(t.type.moveInterval, equals(500));
     });
 
     test('Custom tile tests', () {
