9be9b8a4cb96e3d14aac4b824fe15a6e5c43151d
Chris Norman <chris.norman2@googlemail.com>
2021-10-15 14:33:15 +0100

    Added the `DialogueLevel` class.

---
+++ lib/src/levels/dialogue_level.dart
@@ -0,0 +1,113 @@
+/// Provides the [DialogueLevel] class.
+import 'package:dart_sdl/dart_sdl.dart';
+
+import '../game.dart';
+import '../json/message.dart';
+import '../sound/ambiance.dart';
+import '../sound/events/playback.dart';
+import '../sound/events/sound_channel.dart';
+import '../sound/random_sound.dart';
+import 'level.dart';
+
+/// A level that shows a series of [Message] instances to the player, allowing
+/// them to skip between them.
+class DialogueLevel extends Level {
+  /// Create an instance.
+  DialogueLevel(
+      {required Game game,
+      required this.messages,
+      required this.onDone,
+      this.progressScanCode,
+      this.progressControllerButton,
+      this.position = 0,
+      this.soundChannel,
+      List<Ambiance>? ambiances,
+      List<RandomSound>? randomSounds})
+      : assert(
+            progressScanCode != null || progressControllerButton != null,
+            'Both `ProgressControllerButton` and `progressScanCode` cannot be '
+            '`null`.'),
+        assert(messages.where((element) => element.keepAlive == false).isEmpty,
+            'All messages must have their `keepAlive` value set to `true`.'),
+        super(game, ambiances: ambiances, randomSounds: randomSounds);
+
+  /// The list of messages to progress through.
+  final List<Message> messages;
+
+  /// The function that will be called when the [messages] list has been
+  /// exhausted.
+  final void Function() onDone;
+
+  /// The current position in the [messages] list.
+  ///
+  /// This value will be incremented by the [progress] method.
+  int position;
+
+  /// The sound channel to output sounds through.
+  final SoundChannel? soundChannel;
+
+  /// The currently playing sound.
+  PlaySound? _sound;
+
+  /// The currently-playing sound.
+  PlaySound? get sound => _sound;
+
+  /// The scancode that allows progressing through the [messages] list.
+  final ScanCode? progressScanCode;
+
+  /// The game controller button that lets the player progress through the
+  /// [messages] list.
+  final GameControllerButton? progressControllerButton;
+
+  /// Progress through the list of [messages].
+  ///
+  /// If the focus moves past the end of the list, then [onDone] is called.
+  void progress() {
+    if (position >= messages.length) {
+      onDone();
+    } else {
+      _sound = game.outputMessage(messages.elementAt(position),
+          oldSound: _sound, soundChannel: soundChannel);
+      position++;
+    }
+  }
+
+  @override
+  void onPush() {
+    super.onPush();
+    progress();
+  }
+
+  @override
+  void onPop(double? fadeLength) {
+    super.onPop(fadeLength);
+    final sound = _sound;
+    if (sound != null) {
+      if (fadeLength == null) {
+        sound.destroy();
+      } else {
+        game.registerTask((fadeLength * 1000).round(), sound.destroy);
+      }
+    }
+    _sound = null;
+  }
+
+  @override
+  void handleSdlEvent(Event event) {
+    final button = progressControllerButton;
+    final scanCode = progressScanCode;
+    if (button != null &&
+        event is ControllerButtonEvent &&
+        event.state == PressedState.pressed &&
+        event.button == button) {
+      progress();
+    } else if (scanCode != null &&
+        event is KeyboardEvent &&
+        event.state == PressedState.pressed &&
+        event.repeat == false &&
+        event.key.modifiers.isEmpty &&
+        event.key.scancode == scanCode) {
+      progress();
+    }
+  }
+}
---
+++ test/levels/dialogue_level_test.dart
@@ -0,0 +1,149 @@
+import 'package:dart_sdl/dart_sdl.dart';
+import 'package:test/test.dart';
+import 'package:ziggurat/ziggurat.dart';
+
+import '../helpers.dart';
+
+void main() {
+  group('DialogueLevel', () {
+    test('Initialise', () {
+      final game = Game('DialogueLevel');
+      var done = 0;
+      expect(
+          () => DialogueLevel(game: game, messages: [], onDone: () => done++),
+          throwsA(predicate((value) =>
+              value is AssertionError &&
+              value.message ==
+                  'Both `ProgressControllerButton` and `progressScanCode` '
+                      'cannot be `null`.')));
+      const progressControllerButton = GameControllerButton.a;
+      const progressScanCode = ScanCode.SCANCODE_RETURN;
+      final dialogueLevel = DialogueLevel(
+          game: game,
+          messages: [],
+          onDone: () => done++,
+          progressControllerButton: progressControllerButton,
+          progressScanCode: progressScanCode);
+      expect(dialogueLevel.messages, isEmpty);
+      expect(dialogueLevel.progressControllerButton,
+          equals(GameControllerButton.a));
+      expect(dialogueLevel.progressScanCode, equals(ScanCode.SCANCODE_RETURN));
+      expect(dialogueLevel.position, isZero);
+    });
+    test('.progress', () {
+      final game = Game('DialogueLevel.progress');
+      var done = 0;
+      const message1 = Message(keepAlive: true);
+      const message2 = Message(keepAlive: true);
+      final dialogueLevel = DialogueLevel(
+          game: game,
+          messages: [message1, message2],
+          onDone: () {
+            done++;
+            game.popLevel();
+          },
+          progressControllerButton: GameControllerButton.a);
+      expect(done, isZero);
+      game.pushLevel(dialogueLevel);
+      expect(done, isZero);
+      expect(dialogueLevel.position, equals(1));
+      dialogueLevel.progress();
+      expect(done, isZero);
+      expect(dialogueLevel.position, equals(2));
+      dialogueLevel.progress();
+      expect(done, equals(1));
+      expect(dialogueLevel.position, equals(2));
+      expect(game.currentLevel, isNull);
+    });
+    test('.handleSdlValue', () {
+      final sdl = Sdl();
+      final game = Game('DialogueLevel.handleSdlValue');
+      var done = 0;
+      const message1 =
+          Message(keepAlive: true, sound: AssetReference.file('file1.wav'));
+      const message2 =
+          Message(keepAlive: true, sound: AssetReference.file('file2.wav'));
+      const message3 =
+          Message(keepAlive: true, sound: AssetReference.file('file3.wav'));
+      final dialogueLevel = DialogueLevel(
+          game: game,
+          messages: [message1, message2, message3],
+          onDone: () => done++,
+          progressScanCode: ScanCode.SCANCODE_RETURN,
+          progressControllerButton: GameControllerButton.a);
+      expect(done, isZero);
+      expect(dialogueLevel.sound, isNull);
+      expect(dialogueLevel.position, isZero);
+      game.pushLevel(dialogueLevel);
+      // Shouldn't work because we require `PressedState.pressed`.
+      dialogueLevel.handleSdlEvent(makeKeyboardEvent(
+          sdl, dialogueLevel.progressScanCode!, KeyCode.keycode_RETURN));
+      expect(done, isZero);
+      expect(dialogueLevel.position, equals(1));
+      var sound = dialogueLevel.sound!;
+      expect(sound.sound, equals(message1.sound));
+      expect(sound.channel, equals(game.interfaceSounds.id));
+      // Works because `state == PressedState.pressed`.
+      dialogueLevel.handleSdlEvent(makeKeyboardEvent(
+          sdl, dialogueLevel.progressScanCode!, KeyCode.keycode_RETURN,
+          state: PressedState.pressed));
+      expect(done, isZero);
+      expect(game.currentLevel, equals(dialogueLevel));
+      expect(dialogueLevel.position, equals(2));
+      expect(dialogueLevel.sound, isNot(equals(sound)));
+      sound = dialogueLevel.sound!;
+      expect(sound.sound, equals(message2.sound));
+      expect(sound.channel, equals(game.interfaceSounds.id));
+      // Won't work.
+      dialogueLevel.handleSdlEvent(makeControllerButtonEvent(
+          sdl, dialogueLevel.progressControllerButton!));
+      expect(done, isZero);
+      expect(game.currentLevel, equals(dialogueLevel));
+      expect(dialogueLevel.position, equals(2));
+      expect(dialogueLevel.sound, equals(sound));
+      // Works.
+      final event = makeControllerButtonEvent(
+          sdl, dialogueLevel.progressControllerButton!,
+          state: PressedState.pressed);
+      dialogueLevel.handleSdlEvent(event);
+      expect(done, isZero);
+      expect(game.currentLevel, equals(dialogueLevel));
+      expect(dialogueLevel.position, equals(3));
+      expect(dialogueLevel.sound, isNot(equals(sound)));
+      sound = dialogueLevel.sound!;
+      expect(sound.sound, equals(message3.sound));
+      expect(sound.channel, equals(game.interfaceSounds.id));
+      // Let's make sure `onDone` gets called.
+      dialogueLevel.handleSdlEvent(event);
+      expect(done, equals(1));
+      expect(game.currentLevel, equals(dialogueLevel));
+      expect(dialogueLevel.position, equals(3));
+      expect(dialogueLevel.sound, equals(sound));
+    });
+    test('.onPush', () {
+      final game = Game('DialogueLevel.onPush');
+      const message =
+          Message(keepAlive: true, sound: AssetReference.file('test.wav'));
+      var dialogueLevel = DialogueLevel(
+          game: game,
+          messages: [message],
+          onDone: () {},
+          progressControllerButton: GameControllerButton.a);
+      game.pushLevel(dialogueLevel);
+      var sound = dialogueLevel.sound!;
+      expect(sound.channel, equals(game.interfaceSounds.id));
+      expect(sound.sound, equals(message.sound));
+      final soundChannel = game.createSoundChannel();
+      dialogueLevel = DialogueLevel(
+          game: game,
+          messages: [message],
+          onDone: () {},
+          progressControllerButton: GameControllerButton.a,
+          soundChannel: soundChannel);
+      game.pushLevel(dialogueLevel);
+      sound = dialogueLevel.sound!;
+      expect(sound.channel, equals(soundChannel.id));
+      expect(sound.sound, equals(message.sound));
+    });
+  });
+}
