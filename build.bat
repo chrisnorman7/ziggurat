9987ac8f3a12c470eff3094d244304ce435e815c
Chris Norman <chris.norman2@googlemail.com>
2023-05-17 04:52:55 +0100

    Update build scripts.

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
 @echo off
-dart run build_runner build
+dart run build_runner build --delete-conflicting-outputs
--- watch.bat
+++ watch.bat
@@ -1,2 +1,2 @@
 @echo off
-flutter pub run build_runner watch --delete-conflicting-outputs
+dart run build_runner watch --delete-conflicting-outputs
